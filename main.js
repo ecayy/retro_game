0cbdbaadbc9415221fb981a98fc2db9d4aba9a02
Ezgi <144616221+CerseiLeo@users.noreply.github.com>
2024-08-29 15:38:49 +0300

    .

---
+++ main.js
@@ -0,0 +1,18 @@
+class Player{
+
+}
+
+class Projecttile{
+
+}
+class Enemy{
+
+}
+
+class Game{
+
+}
+
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas')
+});
